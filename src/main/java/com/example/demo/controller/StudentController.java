9430f79e10b2410196b57bd8912f9b422c59b2db
s26963-pj <s26963@pjwstk.edu.pl>
2024-01-23 19:41:19 +0100

    Added StudentController.java Class with simple ResponseEntity

---
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import com.example.demo.exceptions.ValidationException;
+import com.example.demo.model.Student;
+import com.example.demo.service.StudentService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/student")
+@AllArgsConstructor
+public class StudentController {
+    private StudentService studentService;
+
+    @PostMapping
+    public ResponseEntity<Student> addStudent(@RequestBody Student student){
+        try{
+            Student result = studentService.add(student);
+
+            return ResponseEntity.status(201).body(result);
+        }catch (ValidationException e){
+            return ResponseEntity.badRequest().build();
+        }
+    }
+}
